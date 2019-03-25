95155aa860a578c13086e64ea5ffd2df5d51c188
Deborah Barcenas <deborah.barcenas@gmail.com>
2019-03-25 18:24:38 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -12,7 +12,7 @@ def first_challenge
       favorite_icecream_flavors: ["strawberry", "cookie dough", "mint chip"]
     }
   }
-  
+
   contacts["Freddy Mercury"][:favorite_icecream_flavors].delete("strawberry")
     #remember to return your newly altered contacts hash!
     contacts
