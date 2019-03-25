c88c0f660cecfded512c9ef2653469d6f6900f49
Deborah Barcenas <deborah.barcenas@gmail.com>
2019-03-25 18:23:00 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -17,17 +17,13 @@ def first_challenge
     data.each do |attribute, value|
       if attribute == :favorite_icecream_flavors
         value.each do |flavor|
-          flavor.delete_if "strawberry"
+          flavor.delete_if("strawberry")
         end
       end
     end
   end
-
-
+  
+  
     #remember to return your newly altered contacts hash!
     contacts
   end
-
-  #remember to return your newly altered contacts hash!
-  contacts
-end
